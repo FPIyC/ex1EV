8da5a60b68499ed8b0b4350f353380c5d75e4beb
ASIR <ASIR@PC09>
2019-11-28 10:19:51 +0100

    Merge origin/master
    Conflicts:
    	nbproject/project.xml

---
+++ Alvaro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Examen Ej 3</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <p> El texto muestra un numero 22 que parece estar preparado para unas gafas 3d</p>
+        <img src="imagenTexto.jfif"/>
+
+    </body>
+</html>
