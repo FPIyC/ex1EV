b6d492d89765994ce278475408d1adaefff04ad6
FPIyC <57707626+FPIyC@users.noreply.github.com>
2019-11-28 09:52:02 +0100

    Add files via upload

---
+++ RaulAsir.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+A continuación vamos a decirle al profesor que es lo que hemos visto en 
+la imagen descargada del repositorio GitHub
+-->
+//
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo 'La imagen muestra un 22 desordenado y algo borroso,¿Será una imagen 3D?';
+        ?>
+    </body>
+</html>
